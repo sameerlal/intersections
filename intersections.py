52c7e450456b8d0093ad23e1ca636f7d57e14094
Sameer Lal <sj_lal@hotmail.com>
2015-11-01 12:06:31 -0500

    need to fix delimiters

--- intersections.py
+++ intersections.py
@@ -1,2 +1,21 @@
-import datagen
+from numpy import genfromtxt
 
+
+stuff = open('sample1.txt','r')
+Adata =[]
+Bdata =[]
+Cdata =[]
+Ddata = []
+
+my_data = genfromtxt('sample1.txt', delimiter = ',')
+
+print my_data
+
+
+# num_line = sum(1 for line in open('sample1.txt')) 
+# for i in range(1,num_line):
+# 	hello = 3+i
+
+
+
+	
